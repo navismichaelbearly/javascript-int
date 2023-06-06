df42c0b5d57f7be3259621294bb9e95e60f3542d
navismichaelbearly <navis.programmer@gmail.com>
2023-06-06 14:42:09 +0530

    update description

--- liskov-substitution-principle-before.js
+++ liskov-substitution-principle-before.js
@@ -1,3 +1,10 @@
+/*
+The Liskov Substitution Principle (LSP) is a principle in object-oriented programming 
+that states that objects of a superclass should be replaceable with objects of its 
+subclasses without affecting the correctness of the program. In other words, 
+a subclass should be able to be used wherever its superclass is expected.
+*/
+
 class Bird {
     fly() {
         console.log('I can fly')
